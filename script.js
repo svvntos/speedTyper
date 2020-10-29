de68253ad1a8fc3a3d3a8e0ca638dcac638e9962
Joe Santos <jaysantos012@gmail.com>
2020-10-28 23:26:50 -0400

    added more JS for the word poping up, the time, and the score. Missing the adding time, and difficulty levels

---
+++ script.js
@@ -0,0 +1,106 @@
+const word = document.getElementById('word');
+const text = document.getElementById('text');
+const scoreEl = document.getElementById('score');
+const timeEl = document.getElementById('time');
+const endgameEl = document.getElementById('end-game');
+const settingsBtn = document.getElementById('settings-btn');
+const settings = document.getElementById('settings');
+const settingsForm = document.getElementById('settings-form');
+const difficultySelect = document.getElementById('difficulty');
+
+// List of word for the game
+
+const words = [
+    'sigh',
+    'tense',
+    'airplane',
+    'ball',
+    'pies',
+    'juice',
+    'warlike',
+    'bad',
+    'north',
+    'dependent',
+    'steer',
+    'silver',
+    'highfalutin',
+    'superficial',
+    'quince',
+    'eight',
+    'feeble',
+    'admit',
+    'drag',
+    'loving'
+];
+
+// Init word
+let randomWord;
+
+// Init score
+let score = 0;
+
+// Init time
+let time = 10;
+
+// Focus on text on start
+text.focus();
+
+// Start counting down
+
+const timeInterval = setInterval(updateTime, 1000);
+
+// function to get random word 
+function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
+}
+
+// add word to DOM
+function addWordToDom() {
+    randomWord = getRandomWord();
+    word.innerHTML = randomWord;
+}
+// Update Score
+
+function updateScore() {
+    score++;
+    scoreEl.innerHTML = score;
+
+
+
+}
+
+function updateTime() { 
+    time--;
+    timeEl.innerHTML = time + 's';
+
+    if(time === 0) {
+        clearInterval(timeInterval);
+        // end game
+        gameOver();
+    }
+
+
+}
+
+// Game Over, show
+addWordToDom();
+
+
+
+
+//Event Listeners
+text.addEventListener('input', e => {
+    const insertedText = e.target.value;
+
+    if( insertedText === randomWord) {
+        addWordToDom();
+        updateScore();
+        // clear
+        e.target.value = '';
+
+    }
+});
+
+
+
+
