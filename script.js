5467c9dd2a69db94645fbdb5e4430a5eb4326f0a
Joe Santos <jaysantos012@gmail.com>
2020-11-09 18:28:53 -0500

    Finished the difficulty with the game, game is ready to deploy

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const word = document.getElementById('word');
 const text = document.getElementById('text');
 const scoreEl = document.getElementById('score');
 const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game');
+const endgameEl = document.getElementById('end-game-container');
 const settingsBtn = document.getElementById('settings-btn');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
@@ -41,6 +41,16 @@ let score = 0;
 
 // Init time
 let time = 10;
+
+let difficulty = localStorage.getItem('difficulty') !== null ? 
+localStorage.getItem('difficulty') : 'medium';
+
+
+//set difficulty select value
+
+difficultySelect.value = 
+localStorage.getItem('difficulty') !== null ? 
+localStorage.getItem('difficulty') : 'medium';
 
 // Focus on text on start
 text.focus();
@@ -82,7 +92,19 @@ function updateTime() {
 
 }
 
-// Game Over, show
+// Game Over, show end screen
+function gameOver() {
+    endgameEl.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+    `;
+
+    endgameEl.style.display ='flex';
+}
+
+
+
 addWordToDom();
 
 
@@ -97,10 +119,33 @@ text.addEventListener('input', e => {
         updateScore();
         // clear
         e.target.value = '';
+        
+        if(difficulty === 'hard') {
+            time +=2;
+        } else if (difficulty === 'medium') {
+            time += 3;
+        }else {
+            time += 5;
+        }
+
+
+
+        updateTime();
 
     }
 });
 
+
+// Setting btn click
+
+settingsBtn.addEventListener('click', () => 
+settings.classList.toggle('hide'));
+
+settingsForm.addEventListener('change', e => {
+    difficulty = e.target.value;
+    localStorage.setItem('difficulty', difficulty);
+
+})
 
 
 
